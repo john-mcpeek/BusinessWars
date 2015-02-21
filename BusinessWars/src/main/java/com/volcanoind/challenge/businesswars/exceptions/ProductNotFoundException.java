6b6849164a01b5303e544cba2d2225860b72480b
john-mcpeek <juan.mcpeek@gmail.com>
2015-02-21 08:10:36 -0500

    Merge branch 'master' of https://github.com/john-mcpeek/BusinessWars.git

--- BusinessWars/src/main/java/com/volcanoind/challenge/businesswars/exceptions/ProductNotFoundException.java
+++ BusinessWars/src/main/java/com/volcanoind/challenge/businesswars/exceptions/ProductNotFoundException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 public class ProductNotFoundException extends ResponseCodeException {
 	
 	public ProductNotFoundException() {
-		super( HttpStatus.BAD_REQUEST, "We don't sell that sort of deviant paraphernalia around here. You sicko!" );
+		super( HttpStatus.NOT_FOUND, "We don't sell that sort of deviant paraphernalia around here. You sicko!" );
 	}
 }
